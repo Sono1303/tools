368ffca9070aa4445ec5bab19e1b752dffe8e97f
Sono1303 <133033896+Sono1303@users.noreply.github.com>
2024-06-25 15:54:36 +0700

    Update resize_50mb.py

--- resize_50mb.py
+++ resize_50mb.py
@@ -1,3 +1,4 @@
+import os
 from PIL import Image
 
 # Tăng giới hạn kích thước ảnh
@@ -8,6 +9,13 @@ def reduce_image_size(input_path, output_path, target_size_mb, quality=90):
         # Mở ảnh
         image = Image.open(input_path)
         
+        max_size_bytes = target_size_mb * 1024 * 1024
+        image_size = os.path.getsize(input_path)
+        if image_size <= max_size_bytes:
+            image.save(output_path, quality=quality, optimize=True)
+            print(f"Ảnh đã được lưu với kích thước giảm tại: {output_path}, dung lượng: {os.path.getsize(output_path) / (1024 * 1024):.2f} MB")
+            return
+
         # Giảm kích thước ảnh bằng cách downscaling nhưng vẫn giữ nguyên tỷ lệ
         width, height = image.size
         scale_factor = (target_size_mb * 1024 * 1024) / (width * height * 2)  # 3 bytes per pixel for RGB
@@ -32,13 +40,3 @@ def reduce_image_size(input_path, output_path, target_size_mb, quality=90):
         print(f"Ảnh đã được lưu với kích thước giảm tại: {output_path}, dung lượng: {os.path.getsize(output_path) / (1024 * 1024):.2f} MB")
     except Exception as e:
         print(f"Đã xảy ra lỗi: {e}")
-
-import os
-
-# Đường dẫn ảnh đầu vào và đầu ra
-# input_path = '/Users/mac/Documents/resize iamge/TP-HOABINH-2030.jpg'
-# output_path = '/Users/mac/Documents/resize iamge/hoabinh/TP-HOABINH-2030.jpg'
-# target_size_mb = 20  # Kích thước mục tiêu (MB)
-
-# Gọi hàm để giảm kích thước tệp ảnh
-# reduce_image_size(input_path, output_path, target_size_mb)
