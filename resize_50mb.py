cd90c0db6305b717facadb622b7b1d5c175bd79c
Sono1303 <133033896+Sono1303@users.noreply.github.com>
2024-06-25 20:50:40 +0700

    Update resize_50mb.py

--- resize_50mb.py
+++ resize_50mb.py
@@ -4,7 +4,7 @@ from PIL import Image
 # Tăng giới hạn kích thước ảnh
 Image.MAX_IMAGE_PIXELS = None
 
-def reduce_image_size(input_path, output_path, target_size_mb, quality=90):
+def reduce_image_size(input_path, output_path, target_size_mb=50, quality=90):
     try:
         # Mở ảnh
         image = Image.open(input_path)
@@ -12,20 +12,22 @@ def reduce_image_size(input_path, output_path, target_size_mb, quality=90):
         max_size_bytes = target_size_mb * 1024 * 1024
         image_size = os.path.getsize(input_path)
         if image_size <= max_size_bytes:
-            image.save(output_path, quality=quality, optimize=True)
+            image.save(output_path, quality=100, optimize=True)
             print(f"Ảnh đã được lưu với kích thước giảm tại: {output_path}, dung lượng: {os.path.getsize(output_path) / (1024 * 1024):.2f} MB")
             return
 
         # Giảm kích thước ảnh bằng cách downscaling nhưng vẫn giữ nguyên tỷ lệ
         width, height = image.size
-        scale_factor = (target_size_mb * 1024 * 1024) / (width * height * 2)  # 3 bytes per pixel for RGB
-        new_width = int(width * (scale_factor ** 0.2))
-        new_height = int(height * (scale_factor ** 0.2))
-        
-        if new_width < 1 or new_height < 1:
-            raise ValueError("Kích thước ảnh quá nhỏ sau khi giảm.")
+        scale_factor = (max_size_bytes / image_size) ** 0.5
+        new_width = int(width * scale_factor)
+        new_height = int(height * scale_factor)
         
         resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+        
+        # if new_width < 1 or new_height < 1:
+        #     raise ValueError("Kích thước ảnh quá nhỏ sau khi giảm.")
+        
+        # resized_image = image.resize((new_width, new_height), Image.LANCZOS)
         
         # Lưu ảnh với chất lượng giảm
         resized_image.save(output_path, quality=quality, optimize=True)
