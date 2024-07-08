01b9d7e0de0ab423a25f5ba12e2a9c738dc24e99
Sono1303 <133033896+Sono1303@users.noreply.github.com>
2024-07-09 00:39:34 +0700

    Add files via upload

--- advanced_crop_tool.py
+++ advanced_crop_tool.py
@@ -10,14 +10,15 @@ INFINITE = math.inf
 Image.MAX_IMAGE_PIXELS = None
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-INPUT_PATH = r"E:\tools\input\lacthuy\LACTHUY-2020.jpg"
+INPUT_PATH = r"E:\tools\input\caophong\CAOPHONG-2021.jpg"
 OUTPUT_PATH = r"E:\tools\output"
 
 TARGET_COLOR = [(234, 166, 255), (255, 171, 255), (255, 170, 255), (254, 172, 252), (255, 169, 255), 
                 (242, 167, 255), (255, 167, 255), (254, 171, 254), (253, 172, 253), (254, 172, 254),
                 (249, 175, 250), (254, 172, 253), (233, 190, 233), (234, 203, 233), (249, 236, 251),
                 (252, 166, 251), (246, 177, 247), (54, 44, 55), (46, 39, 46), (52, 43, 53), (98, 98, 97),
-                (106, 107, 106), (95, 72, 101), (123, 92, 132), (230, 167, 252)]
+                (106, 107, 106), (95, 72, 101), (123, 92, 132), (230, 167, 252), (81, 61, 86), (107, 81, 115),
+                (116, 87, 122), (85, 65, 92), (151, 118, 118)]
 
 # Hàm tìm tọa độ các pixel của màu viền
 def find_color_pixels(image_path, target_color):
@@ -64,10 +65,17 @@ def advanced_crop_img(input_path, output_path):
         if ext.lower() not in supported_formats:
             raise ValueError(f"Unsupported file format: {ext}")
         
-        # Resize ảnh
+        # Lấy tên ảnh và tạo đường dẫn output
         img_name = os.path.basename(input_path)
         output = f"{output_path}\{img_name}"
-        reduce_image_size(input_path, output)
+
+        # Resize ảnh
+        img = Image.open(input_path)
+        width, height = img.size
+        if (width < 20000 and height < 20000):
+            img.save(output)
+        else:
+            reduce_image_size(input_path, output)
         print("Done resizing")
 
         # Remove background
@@ -83,7 +91,7 @@ def advanced_crop_img(input_path, output_path):
 
         # Crop ảnh và lưu ảnh
         image = Image.open(output)
-        img_cropped = image.crop((left - 40, top -30, right + 40, bottom + 30))
+        img_cropped = image.crop((left - 20, top - 15, right + 20, bottom + 15))
         img_cropped.save(output, format="PNG")
         print("Done cropping")
 
